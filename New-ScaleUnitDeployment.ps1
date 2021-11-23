be68b17eef27fd6bcedf59170facdd1a61e6ebcf
Djordje <petrovicdjordje750@gmail.com>
2021-11-23 02:12:06 +0100

    Updates

--- New-ScaleUnitDeployment.ps1
+++ New-ScaleUnitDeployment.ps1
@@ -1,6 +1,6 @@
 # Deploying scale unit case and exchange rate with bundled components
 # 
-# sof-case-we-dit
+#  sof-case-we-dit
 #  sof: software
 #  case: scale unit Name
 #  we: region west europe
@@ -11,8 +11,7 @@ $templateName = "sof-case-we-dit_deployment"
 $templateFile = "$PSScriptRoot/linked_templates/sof-exchangerate.json"
 $parameterFile =  "$PSScriptRoot/linked_templates/sof-exchangerate-dit-parameters.json"
 
-#Write-Host "Clearning existing resources..."
-#Remove-AzResourceGroup -Name $resourceGroup
+
 Write-Host "Creating resource group..."
 New-AzResourceGroup -Name $resourceGroup -Location westeurope
 
